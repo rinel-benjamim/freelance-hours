c36ab30d4648b411e09392987a3aeb56dc5f56e0
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-10-12 22:51:13 +0100

    work in progress

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/outra-rota', fn() => 'Teste de rota');
+Route::get('/', WelcomeController::class);
