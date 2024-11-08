9c70643f76a23ebf6c1cd09f9f59a4601dd73605
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-08 00:19:36 +0000

    Error solved

--- database/migrations/2024_11_07_230025_create_projects_table.php
+++ database/migrations/2024_11_07_230025_create_projects_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->datetime('ends_at');
             $table->string('status')->default('open');
             $table->json('tech_stack');
-            $table->foreignIdFor(User::class, 'created_by')->constrained();
+            $table->foreignIdFor(User::class, 'created_by')->constrained('users');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Project
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
