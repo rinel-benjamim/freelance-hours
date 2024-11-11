d9f038c7a7700177d2a649a6dd3f6b7549c93735
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-11 20:19:02 +0000

    Update web.php
    rotas base atualizadas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'projects.index');
-
+Route::view('/', 'projects.index')->name('projects.index');
+Route::view('/project/{project}', 'projects.show')->name('projects.show');
