2b9f5cbc091e1fb8f8093944f91c876c0b3d8a94
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-12 18:08:54 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'projects.index')->name('projects.index');
-Route::view('/project/{project}', 'projects.show')->name('projects.show');
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
+Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
