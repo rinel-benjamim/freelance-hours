f2c9c7f60c0460ddc84779e11e1dd03d9c52d4ec
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-12 18:08:22 +0000

    Update Index.php

--- app/Livewire/Projects/Index.php
+++ app/Livewire/Projects/Index.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Projects;
 
+use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
@@ -9,5 +11,13 @@ class Index extends Component
     public function render()
     {
         return view('livewire.projects.index');
+    }
+
+
+    #[Computed()]
+    
+    public function projects()
+    {
+        return Project::query()->inRandomOrder()->get();
     }
 }
