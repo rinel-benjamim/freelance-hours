645f14b07173a74255707e80409bca38d4b9ff34
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-17 20:25:37 +0000

    Timer added
    Adicionado o sistema de live timing da propostas

--- resources/views/components/projects/simple-card.blade.php
+++ resources/views/components/projects/simple-card.blade.php
@@ -16,15 +16,9 @@
             </div>
 
         </div>
-        <div class="flex items-center justify-between mt-4 text-[14px]">
-            <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
-            <div class="font-bold flex items-center space-x-1">
-                <span class="text-white ">01</span><span>:</span>
-                <span class="text-white ">12</span><span>:</span>
-                <span class="text-white ">26</span><span>:</span>
-                <span class="text-white ">64</span>
-            </div>
-        </div>
+
+        <livewire:projects.timer :$project />
+        
     </div>
 
 
@@ -33,12 +27,7 @@
         <div class="uppercase font-bold text-[#8C8C9A] text-[12px]">Tecnologias</div>
         <div class="flex gap-[8px] items-center pb-2">
             @foreach ($project->tech_stack as $tech)
-    
-            <x-ui.tech 
-                :icon="$tech" 
-                :text="$tech" 
-            />
-
+                <x-ui.tech :icon="$tech" :text="$tech" />
             @endforeach
         </div>
     </div>
