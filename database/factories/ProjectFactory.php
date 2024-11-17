190ac26f52a390bd20718c7fe366286c36d99b4c
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-17 21:23:16 +0000

    Update ProjectFactory.php

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -19,8 +19,28 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->words(5, true),
-            'description' => 'oi',
+            'title' => fake()->sentence(5),
+            'description' => '<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulário de Contato</title>
+</head>
+<body>
+    <h1>Entre em Contato</h1>
+    <form>
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome">
+        
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email">
+        
+        <label for="mensagem">Mensagem:</label>
+        <textarea id="mensagem" name="mensagem"></textarea>
+        
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>',
             'ends_at' => fake()->dateTimeBetween('now', '+3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
             'tech_stack' => fake()->randomElements(['nodejs', 'react', 'javascript', 'vite', 'nextjs'], random_int(1, 5)),
