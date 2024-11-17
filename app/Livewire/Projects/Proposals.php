e9c9d9f61a03b1053912aca50732708d46f968f4
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-17 21:23:13 +0000

    Update Proposals.php

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,33 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
+
     public Project $project;
-    
+
+    public int $qty = 5;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()->orderBy('hours')->paginate($this->qty);
+    }
+
+    public function loadMore()
+    {
+        $this->qty += 5;
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
     public function render()
     {
         return view('livewire.projects.proposals');
