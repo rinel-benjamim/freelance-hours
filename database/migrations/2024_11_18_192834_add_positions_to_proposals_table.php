e444127051f8e384aade4c27af46c307d1dc5154
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-11-23 16:44:26 +0000

    arquivo modificado

--- database/migrations/2024_11_18_192834_add_positions_to_proposals_table.php
+++ database/migrations/2024_11_18_192834_add_positions_to_proposals_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('proposals', function (Blueprint $table) {
-            //
+            $table->unsignedInteger('position')->nullable();
+            $table->string('position_status')->nullable();
         });
     }
 
@@ -22,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('proposals', function (Blueprint $table) {
-            //
+            $table->dropColumn(['position', 'position_status']);
         });
     }
 };
